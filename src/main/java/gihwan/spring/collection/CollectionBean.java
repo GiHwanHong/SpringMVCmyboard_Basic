3442e5cc8c4bbbbaeb50dc054908ed0725e0cd84
TJ <TJ@192.168.100.110>
2019-09-08 11:50:08 +0900

    Collection Bean Study Test

---
+++ src/main/java/gihwan/spring/collection/CollectionBean.java
@@ -0,0 +1,14 @@
+package gihwan.spring.collection;
+
+import java.util.List;
+
+public class CollectionBean {
+	private List<String> addressList;
+	
+	public List<String> getAddressList() {
+		return addressList;
+	}
+	public void setAddressList(List<String> addressList) {
+		this.addressList = addressList;
+	}
+}
